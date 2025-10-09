42180d0e9b0b8b6a7dd49681fb2d0f86678be753
sachmans <sachin@builder.ai>
2025-10-09 21:42:22 +0400

    v1.6.1: Version bump for npm publish

--- start-devops-session.sh
+++ start-devops-session.sh
@@ -41,7 +41,7 @@ show_copyright() {
     echo "======================================================================"
     echo
     echo "  CS_DevOpsAgent - Intelligent Git Automation System"
-    echo "  Version 1.6.0 | Build 20251009.12"
+    echo "  Version 1.6.1 | Build 20251009.13"
     echo "  "
     echo "  Copyright (c) 2024 SecondBrain Labs"
     echo "  Author: Sachin Dev Duggal"
